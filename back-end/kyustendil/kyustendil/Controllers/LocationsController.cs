c50e206d7dd328dbf0e7defa418d90b7d4fe2405
Mlgprokn <d.stanislavov@student.fontys.nl>
2021-04-21 09:59:44 +0300

    vsichko se schupi

---
+++ back-end/kyustendil/kyustendil/Controllers/LocationsController.cs
@@ -0,0 +1,30 @@
+﻿using kyustendil.Data;
+using kyustendil.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kyustendil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly MockLocationsRepo _repository = new MockLocationsRepo();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Location>> GetLocations()
+        {
+            return Ok(_repository.GetLocations());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Location> GetLocation(int id)
+        {
+            return Ok(_repository.GetLocation(id));
+        }
+    }
+}
---
+++ back-end/kyustendil/kyustendil/Data/ILocationsRepo.cs
@@ -0,0 +1,15 @@
+﻿using kyustendil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kyustendil.Data
+{
+    public interface ILocationsRepo
+    {
+        IEnumerable<Location> GetLocations();
+
+        Location GetLocation(int id);
+    }
+}
