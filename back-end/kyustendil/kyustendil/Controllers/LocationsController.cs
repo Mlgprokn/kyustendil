f31ff20dbd4342ee32eb9416b20f348c99dcb9cb
Your Name <you@example.com>
2021-04-21 18:10:33 +0300

    Started adding db to asp.net

--- back-end/kyustendil/kyustendil/Controllers/LocationsController.cs
+++ back-end/kyustendil/kyustendil/Controllers/LocationsController.cs
@@ -13,14 +13,34 @@ namespace kyustendil.Controllers
     [ApiController]
     public class LocationsController : ControllerBase
     {
-        private readonly MockLocationsRepo _repository = new MockLocationsRepo();
+        /// <summary>
+        /// Repository
+        /// </summary>
+        private readonly ILocationsRepo _repository;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LocationsController(ILocationsRepo repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// Gets all locations from the database
+        /// </summary>
+        /// <returns>Json file containing all locations</returns>
         [HttpGet]
         public ActionResult<IEnumerable<Location>> GetLocations()
         {
             return Ok(_repository.GetLocations());
         }
 
+        /// <summary>
+        /// Gets single location from the database
+        /// </summary>
+        /// <param name="id">The id of the location</param>
+        /// <returns>Json file containing the location</returns>
         [HttpGet("{id}")]
         public ActionResult<Location> GetLocation(int id)
         {
